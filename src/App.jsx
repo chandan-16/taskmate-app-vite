16642a2aefdfea04caa39f7eeb7aac1f3ceb970b
chandan-16 <chandangautam420.cg@gmail.com>
2026-06-19 16:19:53 +0530

    Created simple task object and display on the page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,34 @@
+import { useState } from 'react'
+
 import './App.css'
 
 function App() {
 
+  const [tasks, setTasks] = useState([
+      {id: 1, name: 'Build React Projects', completed : true},
+      {id: 2, name: 'Play Video Games', completed : false},
+    ]);
+
+  const [show, setShow] = useState(false);
+
+  const handleDelete = (id) => {
+    setTasks(tasks.filter(task => task.id !== id));
+  }
+
+  // onClick={handleDelete(task.id)}
 
   return (
     <>
       <h1>Taskmate App</h1>
+      <button onClick={() => setShow(!show)}>{show ? 'Hide Task' : 'Show Task'}</button>
+      {
+        show && tasks.map((task) => (
+        <li key={task.id}>
+          <span>{task.id}</span> - <span>{task.name}</span>
+          <button onClick={() => handleDelete(task.id)}>Delete</button>
+        </li>
+      ))
+      }
     </>
   )
 }
